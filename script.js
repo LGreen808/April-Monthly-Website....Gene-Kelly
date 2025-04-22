93dfabc097a0dc2d95eef65979dbc33aa21084a5
LGreen808 <526LGreen@frhsd.com>
2025-04-22 14:30:41 +0000

    Working on adding quotes onto the page

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-var myArray = [];
+var myArray = ['Let the stormy clouds chase. Everyone from the place. Come on with the rain. I have a smile on my face',
+    'You dance love, and you dance joy, and you dance dreams.', 'If Fred Astaire is the Cary Grant of dance, I’m the Marlon Brando.', 'Just singing, singing in the rain',
+     'I wanted to do new things with dance, adapt it to the motion picture medium.']
