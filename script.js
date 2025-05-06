3e40e3231c834ef941dc2d688b54083ef4501c06
LGreen808 <526LGreen@frhsd.com>
2025-05-06 14:15:03 +0000

    Working pt 13

---
+++ script.js
@@ -0,0 +1,6 @@
+//Make an intro question asking if someone knows about Gene Kelly!
+const Intro = prompt('Hello and welcome to my website!....Just a question but you happen to know who Gene Kelly is?');
+if (Intro === yes){
+     return prompt('Great! I was curious, sometimes I think im an old soul lol....')
+} else 
+return prompt('Okay, well now you can learn about him!')
